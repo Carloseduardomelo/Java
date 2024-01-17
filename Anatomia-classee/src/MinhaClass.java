2021ddd7ad335528dcdf23ffe0374239cb71d638
carlos eduardo <127193594+Carloseduardomelo@users.noreply.github.com>
2024-01-17 13:12:33 -0300

    estudando java

---
+++ Anatomia-classee/src/MinhaClass.java
@@ -0,0 +1,9 @@
+public class MinhaClass {
+    public static void main (String [] arges) {
+        System.out.println("carlos");
+    };
+
+    public static void test (String [] arges ){
+        System.out.println("namel gay");
+    };  
+}
